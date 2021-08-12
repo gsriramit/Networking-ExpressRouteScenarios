38bc036ac73f082019ddfb0ba274bb4cad92072c
Sriram.G <gsriramit@gmail.com>
2021-08-12 19:28:49 +0530

    Modified the path to the cleanup json

--- ResourceCleanup/Scripts/Cleanup.ps1
+++ ResourceCleanup/Scripts/Cleanup.ps1
@@ -3,7 +3,7 @@
 #Remove all resources by deploying and emtpy template using Complete mode
 $resourceGroupName = 'rg-networking-dev01'
  New-AzResourceGroupDeployment -Name "cleanup-$resourceGroupName" -ResourceGroupName $resourceGroupName `
- -TemplateFile .\Template\Cleanup.json -Mode Complete -Force -AsJob
- 
+ -TemplateFile .\ResourceCleanup\Template\Cleanup.json -Mode Complete -Force -AsJob
+
 #Remove all resource groups
  Remove-AzResourceGroup -Name $resourceGroupName  -Force
